4f40064b2b18f8ad37a0a06240ed0ed19763c9fa
Miguel Lopes <migueldollopes@gmail.com>
2026-03-19 16:00:59 -0300

    Aula 19/03

---
+++ 19-03/src/server.js
@@ -0,0 +1,12 @@
+import express from 'express'
+const app = express()
+app.use(express.json())
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('No routes here')
+})
+
+app.listen(port, () => {
+    console.log(`App running on http://localhost:${port}`)
+})
